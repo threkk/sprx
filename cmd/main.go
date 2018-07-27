0637e0b2d000937dfbbf304e2b59dab9cf317d3b
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2018-07-27 17:06:52 +0200

    wip: roadmap

--- cmd/main.go
+++ cmd/main.go
@@ -11,4 +11,14 @@ func main() {
 	listener, _ := net.Listen("tcp", "localhost:0")
 	log.Println(listener.Addr())
 	log.Fatal(server.Serve(listener))
+	// Format: sprx -p 5200 -link "some.corporate.web:22 > :8022, localhost:8080 > :8080" alberto@myip.local
+	// 1. Start SSH connection. No connection, exit.
+	// 2. Start proxy server. No proxy, exit.
+	// 3. Configure PAC.
+	// 4. Start all the tunnels, starting with the proxy and following with the
+	// PAC and the to the links.
+	// 5. Display the information:
+	//   -> PAC url (localhost:port)
+	//   -> Ports forwarded.
+	//   -> Close with Ctrl + C, Ctrl + D
 }
